57806ca68b140ef7c4249b065449dab3ccf33de6
bulutluoz <abbulutluoz@gmail.com>
2025-05-05 20:52:35 +0200

    4.ders sonu

---
+++ src/day04_methodOlusturmaVeKullanma/C04_IsmiDuzenlemeMethodu.java
@@ -0,0 +1,22 @@
+package day04_methodOlusturmaVeKullanma;
+
+public class C04_IsmiDuzenlemeMethodu {
+
+    public static void main(String[] args) {
+
+        // verilen kullanici ismini
+        // Kullanici 3 isimli olsa da
+        // isimlerin ilk harfi buyuk harf, kalanlar * olacak sekilde yazdirin
+        // Ornek input  : ali mert can
+        //       output : A** M*** C**
+
+
+    }
+
+
+    public static void ismiDuzenleYazdir(String isim){
+
+
+
+    }
+}
