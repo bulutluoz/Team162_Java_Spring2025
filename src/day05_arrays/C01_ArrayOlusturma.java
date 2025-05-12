d62e2bb96a5171d74d567dd611388accb67fcad6
bulutluoz <abbulutluoz@gmail.com>
2025-05-12 19:46:40 +0200

    5.ders sonu

---
+++ src/day05_arrays/C01_ArrayOlusturma.java
@@ -0,0 +1,43 @@
+package day05_arrays;
+
+public class C01_ArrayOlusturma {
+
+
+    public static void main(String[] args) {
+
+
+        int sayi = 2;
+        String str = "Java candir";
+
+        int[] arr = {2,4,7,9};
+
+        String[] isimler = {"Avsar","Ahmet","Nesim","Onur"};
+
+        char harfler[] = {'a','r','y','t','o'};
+        // bu tur kullanimi intelliJ sariya boyayarak bizi uyaririyor
+        // [] isimden sonra degil,
+        // icine konulacak elementlerin data turunden sonra yazilmalidir
+
+        // arr, isimler ve harfler variable'larinin data turu Array'dir
+        // bunlarin array oldugunu bize ve java'ya anlatan [] dir
+        // array'in basinda yazilan int, String ve char ise
+        // array'in icine konulacak elementlerin data turudur
+
+        // 30 kisilik 3-A sinifi olusturalim
+        String[] ucASinifi = new String[30];
+
+        ucASinifi = new String[32];
+
+        ucASinifi = new String[40];
+
+        // icine 5 tane tamsayi koyabilecegimiz bir array olusturun
+        int[] sayilar = new int[5];
+
+
+
+
+
+    }
+}
+
+
